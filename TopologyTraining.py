edc14e6a3bee1d66130dfa260d2717f84ffee980
Xiao <lingxiao1989@gmail.com>
2016-09-13 13:52:19 -0700

    Update TopologyTraining.py

--- TopologyTraining.py
+++ TopologyTraining.py
@@ -1,25 +1,36 @@
 import numpy as np
 import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import sys
 import argparse
 
+
 parser=argparse.ArgumentParser(description='Process the image frame in training or detecting mode')
-parser.add_argument('--train', nargs='+', help='Training mode')
+parser.add_argument('--train', action='store_true', help='Training mode')
 parser.add_argument('--detect', nargs='+', help='Detecting mode')
 args=parser.parse_args()
 
-if args.train is not None:
-	img_path=' '.join(args.train)
+camera = PiCamera()
+camera.resolution = (1024, 768)
+rawCapture = PiRGBArray(camera)
+
+camera.capture(rawCapture, format="bgr")
+img = rawCapture.array
+
+if args.train:
+	#img_path=' '.join(args.train)
 	car_cascade5=cv2.CascadeClassifier('/home/pi/Workspace/ParkMe/HaarCarsXML/cascade.xml')
-	img=cv2.imread(img_path)
+	#img=cv2.imread(img_path)
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 	cars = car_cascade5.detectMultiScale(gray, 1.3, 5)
 	roi=[]
 	for (x,y,w,h) in cars:
-		if h>150 and h<200:
+		if h>100 and h<200:
 			roi.append([x,y,w,h])
-			#cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+		#cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
 	for (x,y,w,h) in roi:
 		gray_crop=gray[y:y+h, x:x+w]
